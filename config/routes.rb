66d77de1bc49d08d88d7cc7846800a1de06fdb8d
Pascal Fodiman <pascal@fodiman.fr>
2020-12-05 15:56:23 +0100

    add edit & update actions

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root  to: 'tasks#home'
   get '/tasks/new', to: 'tasks#new'
   get '/tasks', to: 'tasks#index'
-  get '/tasks/:id/edit', to: 'tasks#show', as: :task
+  get '/tasks/:id', to: 'tasks#show', as: :task
   post '/tasks', to: 'tasks#create'
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit
+  patch '/tasks/:id', to: 'tasks#update'
 end
