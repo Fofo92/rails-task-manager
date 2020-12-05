79efbbb762fb070cf7547cac1b75d613173cc126
Pascal Fodiman <pascal@fodiman.fr>
2020-12-05 13:57:06 +0100

    add show view with title

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,15 @@
 class TasksController < ApplicationController
+  before_action :set_task
   def index
     @tasks = Task.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root  to: 'tasks#home'
   get '/tasks', to: 'tasks#index', as: :restaurants
+  get '/tasks/:id', to: 'tasks#show', as: :restaurant
 end
